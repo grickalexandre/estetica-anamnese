b86c1c10e20d2106d3de7f381a03f1767a20bd48
Luis Oliveira <luis.oliveira@email.com>
2025-10-14 21:32:10 -0300

    Fix: Recriar firebase.js para resolver problema de export no Vercel

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBPd-t8X34vBzGqGM2XTSpxGh8lUdQe4jo",
   authDomain: "estetica-anamnese.firebaseapp.com",
@@ -13,13 +11,9 @@ const firebaseConfig = {
   appId: "1:610199167597:web:5a234a65466614408ad564"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Firestore
 export const db = getFirestore(app);
-
-// Initialize Storage
 export const storage = getStorage(app);
 
 export default app;
